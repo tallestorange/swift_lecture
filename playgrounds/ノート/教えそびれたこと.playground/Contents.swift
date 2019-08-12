08068adeb94e7b6b3e6d5c7507872533fe7b00b7
Yuhel Tanaka <tallestorangeyuhei@gmail.com>
2019-08-12 16:46:05 +0900

    教えそびれたこと関連の資料を追加

---
+++ playgrounds/ノート/教えそびれたこと.playground/Contents.swift
@@ -0,0 +1,83 @@
+enum Weekday {
+    case sunday
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    
+    var name:String { // computed propertyも持てる
+        switch self {
+        case .sunday:
+            return "日"
+        case .monday:
+            return "月"
+        case .tuesday:
+            return "火"
+        case .wednesday:
+            return "水"
+        case .thursday:
+            return "木"
+        case .friday:
+            return "金"
+        case .saturday:
+            return "土"
+        }
+    }
+    
+    init?(japaneseName:String) { // イニシャライザも持てる
+        switch japaneseName {
+        case "月":
+            self = .monday
+        case "火":
+            self = .tuesday
+        case "水":
+            self = .wednesday
+        case "木":
+            self = .thursday
+        case "金":
+            self = .friday
+        case "土":
+            self = .saturday
+        case "日":
+            self = .sunday
+        default: // どれにも該当しなかった場合
+            return nil
+        }
+    }
+}
+
+let day1 = Weekday.sunday
+let day2:Weekday = .wednesday // 型がわかっている場合は省略可能
+let day3 = Weekday(japaneseName: "月")! // イニシャライザを使用した初期化
+
+print(day2.name)
+
+switch day2 {
+// switch文で分岐処理を行う場合は網羅性チェックをしてくれる
+case .sunday:
+    print("日")
+case .monday:
+    print("月")
+case .tuesday:
+    print("火")
+case .wednesday:
+    print("水")
+case .thursday:
+    print("木")
+case .friday:
+    print("金")
+case .saturday:
+    print("土")
+}
+
+enum BloodType:Int {
+    case ab = 0
+    case a
+    case b
+    case o
+}
+
+let bloodType:BloodType = .b
+print(bloodType.rawValue)
